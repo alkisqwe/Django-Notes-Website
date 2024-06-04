321ab8224130d5595b7a7361773fa6d69a43c04e
root <alkisqwe@gmail.com>
2024-06-05 02:08:11 +0300

    Final

--- notes/settings.py
+++ notes/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ws0=od6-iv#_-3tfy%t_)9kv0^(_ib$ci2d#0&zw%_iv8n!7&s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
